aca877a8d0af712e7ecdccac39a081b9b005af23
ju5t <3635751+ju5t@users.noreply.github.com>
2026-06-08 11:46:36 +0000

    chore: fix styling

--- src/Resources/DomainsResource.php
+++ src/Resources/DomainsResource.php
@@ -12,6 +12,6 @@ final class DomainsResource extends BaseResource
     /** @return array<string> */
     public function list(): array
     {
-        return $this->connector->send(new ListDomains())->dto();
+        return $this->connector->send(new ListDomains)->dto();
     }
 }
